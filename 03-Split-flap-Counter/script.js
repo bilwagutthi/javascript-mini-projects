792208c02a965c0d8e494c10a0f85f35ab9116d0
Bilwa Gutthi <bilwa.gutthi@gmail.com>
2021-07-07 22:55:06 +0530

    03 script.js start

---
+++ 03-Split-flap-Counter/script.js
@@ -0,0 +1,41 @@
+window.onload=()=>{
+    
+    const frame= document.getElementById("frame");
+
+
+    function addStartMenu(){
+        let menu= document.createElement("div");
+        menu.className = "startMenu";
+        let form = document.createElement("form")
+        for ( let i=5; i <=60 ; i5++){
+            let temp= document.createElement("input");
+            temp.attributes 
+        }
+        frame.
+    }
+    
+    function menuHTML(){
+        let menu="<div class=\"startMenu\"><form action=\"\">";
+                <input type="radio" name="" id="">
+                <label for="">
+                    <div class="button_box"></div>
+                </label>
+
+        for(let i=1; i<=12;i++){
+            menu+= "<input >"
+        }
+
+        menu+="</form></div>"
+        return menu;
+    }
+    function start(){
+        let time =addStartMenu();
+    }
+    
+    start();
+};
+
+
+
+
+
