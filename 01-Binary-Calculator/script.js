b5adc57c1705a3a8c5fe367d012da89770c4c45f
Bilwa Gutthi <bilwa.gutthi@gmail.com>
2021-07-08 18:51:02 +0530

    01 script.js update

--- 01-Binary-Calculator/script.js
+++ 01-Binary-Calculator/script.js
@@ -2,23 +2,64 @@ document.addEventListener('DOMContentLoaded',()=>{
     const display= document.getElementById("display")
     const num0= document.getElementById("num0")
     const num1= document.getElementById("num1")
+    const clear= document.getElementById("clear")
     const calc= document.getElementById("calc")
     const add= document.getElementById("add")
     const sub= document.getElementById("sub")
-    const mul= document.getElementById("mul")
-    const div= document.getElementById("div")
 
     let isTyping= true
     let isComplete=false
+    let firstNotDone= true
 
     var expression=""
 
     function updateDisplay(){
-        display.text=expression
+        display.textContent=expression
+        console.log(expression)
+    }
+
+    function charCheck(){
+        return expression.length <=17
     }
 
     num0.onclick= ()=>{
+        if(charCheck()){
         expression+="0"
         updateDisplay()
+        }
     }
+
+    num1.onclick= ()=>{
+        if(charCheck()){
+        expression+="1"
+        updateDisplay()
+    }
+    }
+
+    clear.onclick=()=>{
+        expression= ""
+        firstNotDone= true
+        updateDisplay()
+    }
+
+    add.onclick=()=>{
+        if(firstNotDone && charCheck()){
+            expression+="+"
+            firstNotDone= false
+        }
+        updateDisplay()
+    }
+
+    sub.onclick=()=>{
+        if(firstNotDone && charCheck()){
+            expression+="-"
+            firstNotDone= false
+        }
+        updateDisplay()
+    }
+
+    calc.onclick= () => {
+        
+    }
+
 })
