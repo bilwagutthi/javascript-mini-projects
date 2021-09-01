e1399a2385b7fa07ed4645760e0d49611b851fa7
Bilwa Gutthi <bilwa.gutthi@gmail.com>
2021-09-01 19:49:31 +0530

    Binary calc done. Readme to be updated

--- 01-Binary-Calculator/script.js
+++ 01-Binary-Calculator/script.js
@@ -1,90 +1,90 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const display= document.getElementById("display")
-    const num0= document.getElementById("num0")
-    const num1= document.getElementById("num1")
-    const clear= document.getElementById("clear")
-    const calc= document.getElementById("calc")
-    const add= document.getElementById("add")
-    const sub= document.getElementById("sub")
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.getElementById("display")
+    const num0 = document.getElementById("num0")
+    const num1 = document.getElementById("num1")
+    const clear = document.getElementById("clear")
+    const calc = document.getElementById("calc")
+    const add = document.getElementById("add")
+    const sub = document.getElementById("sub")
 
-    let isTyping= true
-    let isComplete=false
-    let firstNotDone= true
+    let firstDone = false
     let oprAdd;
 
-    var expression=""
+    var expression = ""
 
-    function updateDisplay(){
-        display.textContent=expression
-        console.log(expression)
+    function updateDisplay() {
+        display.textContent = expression
+        console.log("exp", expression)
     }
 
-    function charCheck(){
-        return expression.length <=17
+    function charCheck() {
+        return expression.length <= 17
     }
 
-    num0.onclick= ()=>{
-        if(charCheck()){
-        expression+="0"
-        updateDisplay()
+    num0.onclick = () => {
+        if (charCheck()) {
+            expression += "0"
+            updateDisplay()
         }
     }
 
-    num1.onclick= ()=>{
-        if(charCheck()){
-        expression+="1"
-        updateDisplay()
-    }
-    }
-
-    clear.onclick=()=>{
-        expression= ""
-        firstNotDone= true
-        updateDisplay()
+    num1.onclick = () => {
+        if (charCheck()) {
+            expression += "1"
+            updateDisplay()
+        }
     }
 
-    add.onclick=()=>{
-        if(firstNotDone && charCheck()){
-            expression+="+"
-            firstNotDone= false
+    clear.onclick = () => {
+        expression = ""
+        firstDone = false
+        updateDisplay()
+    }
+
+    add.onclick = () => {
+        if (!firstDone && charCheck()) {
+            expression += "+"
+            firstDone = true
             oprAdd = true
         }
         updateDisplay()
     }
 
-    sub.onclick=()=>{
-        if(firstNotDone && charCheck()){
-            expression+="-"
-            firstNotDone= false
+    sub.onclick = () => {
+        if (!firstDone && charCheck()) {
+            expression += "-"
+            firstDone = true
             oprAdd = false
         }
         updateDisplay()
     }
 
-    function evaluateExp(){
-        let oprPos, ans, one , two;
-        if (oprAdd){
+    function evaluateExp() {
+        let oprPos, ans, one, two;
+        if (oprAdd) {
             oprPos = expression.indexOf("+")
         }
-        else{
-            oprPos= expression.indexOf("-")
+        else {
+            oprPos = expression.indexOf("-")
         }
-        one = parseInt(expression.substring(0,oprPos),2)
-        
-        if(oprPos>= expression.length)
-        two = parseInt(expression.substring(oprPos+1),2)
-        else two = 0
-        console.log(oprPos, one , two);
-        if (oprAdd) ans= one+two
-        else ans=Math.abs(one- two)
-        console.log(ans.toString(2),ans)
-        return ans.toString(2)
+        one = parseInt(expression.substring(0, oprPos), 2)
+
+
+        two = (oprPos < expression.length) ? parseInt(expression.substring(oprPos), 2) : 0
+        console.log("opPos", oprPos, "num1", one, "num2", two);
+        if (oprAdd) ans = one + two
+        else ans = Math.abs(one - two)
+
+        ans = ans.toString(2)
+        console.log("Ans", ans)
+        return ans
 
     }
 
-    calc.onclick= () => {
-        if(!firstNotDone && charCheck){
-        expression= evaluateExp()
+    calc.onclick = () => {
+        if (firstDone && charCheck) {
+            expression = evaluateExp()
+            firstDone = false
         }
         updateDisplay()
     }
